d7ccc13b7d39879e0078cf829d9d8eaf3844f0f4
Ryan Roden-Corrent <ryan@rcorre.net>
2020-11-23 21:16:18 -0500

    Ignore steam_proton windows.

--- config.def.h
+++ config.def.h
@@ -26,8 +26,9 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	/* class          instance        title       tags mask     isfloating   monitor */
+	{ "Gimp",         NULL,           NULL,       0,            1,           -1 },
+	{ "steam_proton", "steam_proton", NULL,       0,            1,           -1 },
 };
 
 /* layout(s) */
